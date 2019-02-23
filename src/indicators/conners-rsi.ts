b3be8c258d810d3a7d4e664684173a159adf7e37
Ashley Davis <ashley@codecapers.com.au>
2019-02-23 18:01:33 +1000

    Rewrote Connor's RSI after reading orginal research on the indicator.

--- src/indicators/conners-rsi.ts
+++ src/indicators/conners-rsi.ts
@@ -6,34 +6,26 @@ import '../indicators/rate-of-change';
 
 declare module "data-forge/build/lib/series" {
     interface ISeries<IndexT, ValueT> {
-        crsi(rsiPeriod: number, streakRsiPeriod: number, rocPeriod: number): ISeries<IndexT, number>;
+        crsi(rsiPeriod: number, streakRsiPeriod: number, percentRankPeriod: number): ISeries<IndexT, number>;
     }
 
     interface Series<IndexT, ValueT> {
-        crsi(rsiPeriod: number, streakRsiPeriod: number, rocPeriod: number): ISeries<IndexT, number>;
+        crsi(rsiPeriod: number, streakRsiPeriod: number, percentRankPeriod: number): ISeries<IndexT, number>;
     }
 }
 
-function crsi<IndexT = any>(this: ISeries<IndexT, number>, rsiPeriod: number, streakRsiPeriod: number, rocPeriod: number): ISeries<IndexT, number> {
+function crsi<IndexT = any>(this: ISeries<IndexT, number>, rsiPeriod: number, streakRsiPeriod: number, percentRankPeriod: number): ISeries<IndexT, number> {
     
     assert.isNumber(rsiPeriod, "Expected 'rsiPeriod' parameter to 'DataFrame.crsi' to be a number that specifies the time period for the rsi component of crsi.");
     assert.isNumber(streakRsiPeriod, "Expected 'streakRsiPeriod' parameter to 'DataFrame.crsi' to be a number that specifies the time period for the streak/updown component of crsi.");
-    assert.isNumber(rocPeriod, "Expected 'rocPeriod' parameter to 'DataFrame.crsi' to be a number that specifies the time period for the roc component of crsi.");
+    assert.isNumber(percentRankPeriod, "Expected 'percentRankPeriod' parameter to 'DataFrame.crsi' to be a number that specifies the time period for the roc component of crsi.");
 
-    const totalValues = this.count();
-    const alignedValues = Math.min(totalValues - (rsiPeriod-1), totalValues - 1 - (streakRsiPeriod-1), totalValues - (rocPeriod-1));
     const rsi = this.rsi(rsiPeriod);
     const streaks = this.streaks(2).rsi(streakRsiPeriod);
-    const roc = this.roc(rocPeriod);
-
-    return Series.zip( //TODO: Be good if DF had a function to align/merge values.
-        [
-            rsi.tail(alignedValues), 
-            streaks.tail(alignedValues),
-            roc.tail(alignedValues) 
-        ],
-        values => values.average()
-    );
+    const percentRank = this.amountChange(2).bake().percentRank(percentRankPeriod); //TODO: Don't want to have to do this bake. But %rank is quite slow otherwise.
+    return rsi.merge(streaks, percentRank)
+        .where(values => values.filter(value => value !== undefined).length === 3)
+        .select(([rsi, streak, percentRank]) => (rsi + streak + percentRank)/3);
 }
 
 Series.prototype.crsi = crsi;
