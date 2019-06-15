ee00d011c80b377dbb30699ef348b8265811c806
Ashley Davis <ashley@codecapers.com.au>
2019-06-15 13:38:29 +1000

    Fixed an issue in the ema function.

--- src/indicators/ema.ts
+++ src/indicators/ema.ts
@@ -18,7 +18,10 @@ function ema<IndexT = any>(this: ISeries<IndexT, number>, period: number): ISeri
     // https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp
     const mult = (2 / (period + 1));
     let avgValue = this.take(period).average(); //TODO: this destroy the index.
-    return new Series<IndexT, number>([ avgValue ]) //fio: Shouldn't need the cast here.
+    return new Series<IndexT, number>({
+            index: [ this.getIndex().skip(period-1).first() ], // TODO: The construction of this 1 elements series is quite awkward.
+            values: [ avgValue ],
+        }) 
         .concat(
             this.skip(period)
                 .select(value => {
